3a60b1c0f34d8b54a45a35e10213887f279f133e
Ondra Medek <xmedeko@gmail.com>
2015-12-16 21:36:20 +0100

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace WinProxyViewer
+{
+    public class Program : IDisposable
+    {
+        private static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.ParseArgs(args);
+
+            ProxySettingsViewer viewer = new ProxySettingsViewer(program.TextWriter);
+            viewer.PrintProxyConfiguration();
+            program.Exit();
+        }
+
+        /// <summary>
+        /// Wait for a key when exiting.
+        /// </summary>
+        private bool _exitWait;
+
+        /// <summary>
+        /// Open specified file name on exit.
+        /// </summary>
+        private string _exitOpenFileName;
+
+        public Program()
+        {
+            ProgramName = System.AppDomain.CurrentDomain.FriendlyName;
+            ProgramName = Path.GetFileNameWithoutExtension(ProgramName);
+
+            // output to console by default
+            TextWriter = Console.Out;
+            _exitWait = true;
+        }
+
+        /// <summary>
+        /// Where to write the results.
+        /// </summary>
+        public TextWriter TextWriter { get; private set; }
+
+        /// <summary>
+        /// Name of this executable program file.
+        /// </summary>
+        public string ProgramName { get; private set; }
+
+        /// <summary>
+        /// Parce command line arguments and set the inner variables.
+        /// </summary>
+        private void ParseArgs(string[] args)
+        {
+            if (args == null || args.Length <= 0)
+                return;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+                if (string.IsNullOrEmpty(arg))
+                    continue;
+
+                string argLower = arg.ToLower();
+                if (argLower == "-h")
+                    HelpAndExit();
+
+                if (argLower == "-o")
+                {
+                    if (i >= args.Length - 1)
+                        HelpAndExit();
+                    i++;
+
+                    TextWriter = new StreamWriter(args[i]);
+                    _exitWait = false;
+                    continue;
+                }
+
+                if (argLower == "-tmp")
+                {
+                    string tmp = Path.GetTempFileName();
+                    _exitOpenFileName = Path.ChangeExtension(tmp, "txt");
+                    File.Move(tmp, _exitOpenFileName);
+
+                    TextWriter = new StreamWriter(_exitOpenFileName);
+                    _exitWait = false;
+                    continue;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Write help and exit.
+        /// </summary>
+        private void HelpAndExit()
+        {
+            Console.WriteLine(string.Format("Usage: {0} [-h] [-tmp] [-o filename]", ProgramName));
+            _exitWait = true;
+            Exit();
+        }
+
+        /// <summary>
+        /// Do exit with some actions.
+        /// </summary>
+        private void Exit()
+        {
+            Dispose();
+            if (_exitWait)
+            {
+                Console.WriteLine("Press any key.");
+                Console.ReadKey();
+            }
+
+            if (_exitOpenFileName != null)
+                OpenFile(_exitOpenFileName);
+
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Open specified file.
+        /// </summary>
+        private void OpenFile(string fileName)
+        {
+            try
+            {
+                Process.Start(fileName);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Cannot open " + fileName);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (TextWriter == null || TextWriter == Console.Out)
+                return;
+            TextWriter.Dispose();
+        }
+    }
+}
---
+++ src/ProxySettingsViewer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace WinProxyViewer
+{
+    /// <summary>
+    /// Main class to print proxy settings.
+    /// See http://stackoverflow.com/a/11750887/254109
+    /// </summary>
+    public class ProxySettingsViewer
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WinhttpCurrentUserIeProxyConfig
+        {
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool AutoDetect;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string AutoConfigUrl;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string Proxy;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string ProxyBypass;
+        }
+
+        [DllImport("winhttp.dll", SetLastError = true)]
+        private static extern bool WinHttpGetIEProxyConfigForCurrentUser(ref WinhttpCurrentUserIeProxyConfig pProxyConfig);
+
+        private TextWriter _w;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="w">TextWriter where will be written the output.</param>
+        public ProxySettingsViewer(TextWriter w)
+        {
+            _w = w;
+        }
+
+        /// <summary>
+        /// Start reading proxy config and write it.
+        /// </summary>
+        public void PrintProxyConfiguration()
+        {
+            try
+            {
+                var config = new WinhttpCurrentUserIeProxyConfig();
+                WinHttpGetIEProxyConfigForCurrentUser(ref config);
+
+                _w.WriteLine("Autodetect: " + config.AutoDetect);
+                _w.WriteLine("Proxy: " + config.Proxy);
+                _w.WriteLine("Proxy bypass: " + config.ProxyBypass);
+                _w.WriteLine("Auto config URL: " + config.AutoConfigUrl);
+
+                if (!string.IsNullOrEmpty(config.AutoConfigUrl))
+                {
+                    _w.WriteLine("----- Auto config content -----");
+
+                    Uri configUrl = new Uri(config.AutoConfigUrl);
+
+                    // I do not know if the
+                    PrintUrlContent(configUrl);
+                }
+            }
+            catch (Exception e)
+            {
+                _w.WriteLine("Cannot read proxy config.", e);
+            }
+        }
+
+        /// <summary>
+        /// Prints the content of the URL. Do not use proxy.
+        /// </summary>
+        private void PrintUrlContent(Uri url)
+        {
+            try
+            {
+                WebRequest req = WebRequest.Create(url);
+                req.Proxy = new WebProxy(); // no proxy
+
+                using (WebResponse response = req.GetResponse())
+                using (var stream = response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                    _w.Write(reader.ReadToEnd());
+            }
+            catch (Exception e)
+            {
+                Error("Reading Auto config.", e);
+            }
+        }
+
+        /// <summary>
+        /// Prints error message.
+        /// </summary>
+        /// <param name="msg"></param>
+        private void Error(string msg, Exception e = null)
+        {
+            _w.WriteLine("ERROR " + msg + (e == null ? "" : e.Message));
+        }
+    }
+}
